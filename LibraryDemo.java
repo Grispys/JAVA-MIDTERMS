2e8d8610c3ff612cebb5c9f59c976d0f8465800d
Grispys <grispanimation@gmail.com>
2024-10-23 00:12:18 -0230

    added comments to librarydemo

--- LibraryDemo.java
+++ LibraryDemo.java
@@ -3,6 +3,8 @@ public class LibraryDemo {
     
     @SuppressWarnings("unused")
     public static void main(String[] args) {
+        // libraryitems get their author attribute from the (addItem) function that belongs to authors. it didn't make much sense to have to define an author in the libraryitem
+        // constructor, and then also add the author to the book via that function. so i just have the function. please don't be mad at me :(
         AuthorManagement author1 = new AuthorManagement("Marcus","20-01-1999");
         AuthorManagement author2 = new AuthorManagement("Douglas","11-06-1980");
         Patron patron1 = new Patron("Drake Rose", "12 aberdeen", "958710", "student");
@@ -36,7 +38,7 @@ public class LibraryDemo {
         // edit the library items info
         System.out.println();
         System.out.println();
-        
+
 
         Searcher.search("marcus", patron1);
         Searcher.search("Marcus", patron1);
